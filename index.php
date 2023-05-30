c0c58de79d68ffea52e2d1654ef2fc8090ca4e1f
nelsonmayo0 <nelsonmayomendez5gmail.com>
2023-05-29 22:05:00 -0600

    subiendo proyecto

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        ESCRIBE TU NOMBRE:
+        <form action="nelson.php" method="get">
+            <input type="text" name="nombre">
+            <br>
+            <br>
+            <input type="submit" value="Enviar">
+        </form>
+    </body>
+</html>         
